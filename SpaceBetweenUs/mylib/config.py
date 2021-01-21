4aa6976e7001c9c95caba286a9384e909953b76a
Kiryuumaru <kiryuumaru@outlook.com>
2021-01-21 10:17:16 +0800

    Updated config.py for additional downloadables

--- SpaceBetweenUs/mylib/config.py
+++ SpaceBetweenUs/mylib/config.py
@@ -1,5 +1,5 @@
 # base path to YOLO directory
-MODEL_PATH = "YoloModels"
+MODEL_PATH = "Additionals/YoloModels"
 # initialize minimum probability to filter weak detections along with
 # the threshold when applying non-maxima suppression
 MIN_CONF = 0.3
