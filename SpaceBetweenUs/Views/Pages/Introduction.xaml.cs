edb1651d95d9c7cf66b960f7dcdb07b37fe2ba59
Kiryuumaru <kiryuumaru@outlook.com>
2021-01-28 08:02:58 +0800

    Test modern ui

--- SpaceBetweenUs/Views/Pages/Introduction.xaml.cs
+++ SpaceBetweenUs/Views/Pages/Introduction.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SpaceBetweenUs.Views
+namespace SpaceBetweenUs.Views.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Introduction.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Introduction : UserControl
     {
-        public MainWindow()
+        public Introduction()
         {
             InitializeComponent();
         }
