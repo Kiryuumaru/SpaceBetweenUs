3e847cf1bffdab876d850242cae3d7040460de26
Kiryuumaru <kiryuumaru@outlook.com>
2021-02-23 15:02:21 +0800

    Initial git lfs uninstall

--- Make.bat
+++ Make.bat
@@ -1,6 +1,7 @@
 title Making Repositories
 echo off >nul
-cd %~dp0 >nul
+setlocal >nul
+cd /d %~dp0 >nul
 
 echo .
 echo Cloning repos ...
@@ -13,6 +14,22 @@ git clone https://https://github.com/Kiryuumaru/Alturos.Yolo
 cd Alturos.Yolo
 git submodule update --init
 cd..
+
+title Downloading Large Files
+echo ----------------------------------------------
+echo .
+echo Dowloading additionals large files ...
+echo ----------------------------------------------
+mkdir Additionals
+curl "https://www.googleapis.com/drive/v3/files/1EEIjiuMSmObt37HdKFRpfiQ55Y0yNOtI?alt=media&key=AIzaSyDefGXOc8Ud3eFRAm-53SLbpAFxBqtbfCc" -o "%cd%\Additionals\Additionals.zip"
+echo Unzipping...
+powershell -command "Expand-Archive -Force '%cd%\Additionals\Additionals.zip' '%cd%\Additionals\'"
+echo Copying...
+move "%cd%\Additionals\coco.names" "%cd%\SpaceBetweenUs\Assets\coco.names"
+move "%cd%\Additionals\yolov3.cfg" "%cd%\SpaceBetweenUs\Assets\yolov3.cfg"
+move "%cd%\Additionals\yolov3.weights" "%cd%\SpaceBetweenUs\Assets\yolov3.weights"
+move "%cd%\Additionals\cudnn64_7.dll" "%cd%\SpaceBetweenUs\cudnn64_7.dll"
+pause >nul
 echo ----------------------------------------------
 title Done
 echo Done...
